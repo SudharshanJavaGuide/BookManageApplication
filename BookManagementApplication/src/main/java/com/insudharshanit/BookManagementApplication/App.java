19a3e4b89e6e7a622145bcc636343ded98b95742
Sudharshan <sudharshanjavaguide@gmail.com>
2021-11-11 12:55:01 +0530

    First Commit

---
+++ BookManagementApplication/src/main/java/com/insudharshanit/BookManagementApplication/App.java
@@ -0,0 +1,13 @@
+package com.insudharshanit.BookManagementApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
